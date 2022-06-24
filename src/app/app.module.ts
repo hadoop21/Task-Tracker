8df183fb4ed3915bf56d45591657033f43f58ad6
Uday Kumar <udaysankeneni@gmail.com>
2022-06-24 19:28:26 -0400

    added close button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from '@angular/router';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -8,7 +12,15 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { Router } from 'express';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: TasksComponent}
+]
 
 @NgModule({
   declarations: [
@@ -16,15 +28,20 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    HttpClientModule
-
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -23,7 +23,10 @@ export class TasksComponent implements OnInit {
  toggleReminder(task: Task) {
    task.reminder = !task.reminder;
    this.taskService.updateTaskReminder(task).subscribe()
+ }
 
+ addTask(task: Task) {
+  this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
  }
  }
 
